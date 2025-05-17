565900bc41ac1bd0bce7eaa61ea5653212d2e50b
mig-Rwa <miguelmbabatunga32@gmail.com>
2025-05-17 21:07:31 +0300

    fix: render event images from backend domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['eventflix-backend.onrender.com'],
+  },
+};
 
 module.exports = nextConfig;
